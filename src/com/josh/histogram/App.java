741f832402d3cc052b3ef3b5d88c9919c80829b0
joshuadesmith <joshuadesmith@utexas.edu>
2018-11-30 17:43:19 -0600

    add basic driver code

--- src/com/josh/histogram/App.java
+++ src/com/josh/histogram/App.java
@@ -1,7 +1,23 @@
 package com.josh.histogram;
 
 public class App {
+
+    /**
+     * Driver for the generating a word frequency histogram from an input text file.
+     * The results are written to a file named output.txt.
+     *
+     * TODO: Add ability to choose the name of the output file
+     */
     public static void main(String[] args) {
-        System.out.println("Hey world howzit going");
+        WordHistogram histogram = new WordHistogram();
+
+        String inputFileName = "input.txt";
+        if (args.length > 0) {
+            inputFileName = args[0];
+        }
+
+        histogram.consumeTextFile(inputFileName);
+        histogram.sortDescending();
+        histogram.writeToTextFile();
     }
 }
